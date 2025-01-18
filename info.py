467cba63aeb14f2f394ffaa01390195bb029ca17
PIRO <143400450+PIROXTG@users.noreply.github.com>
2025-01-18 11:43:51 +0530

    sorry  MKN nanba

--- info.py
+++ info.py
@@ -55,10 +55,10 @@ RemoveBG_API = environ.get("RemoveBG_API", "")
 SHORT_URL = environ.get("SHORT_URL")
 SHORT_API = environ.get("SHORT_API")
 
-# Others
-IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "300"))
+# Others0
+IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "120"))
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'MKN_BOTZ_DISCUSSION_GROUP')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '+JFfnvMIjm2g0YmU1')
 P_TTI_SHOW_OFF = is_enabled(environ.get('P_TTI_SHOW_OFF', "True"), True)
 PM_IMDB = is_enabled(environ.get('PM_IMDB', "True"), True)
 IMDB = is_enabled(environ.get('IMDB', "True"), True)
